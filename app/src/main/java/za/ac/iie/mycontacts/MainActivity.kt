2b92378fde615c991a67f85112d3af239dd37903
banzaoceane40-cloud <banzaoceane40@gmail.com>
2026-03-12 12:53:44 +0200

    age until 100 calculation

--- app/src/main/java/za/ac/iie/mycontacts/MainActivity.kt
+++ app/src/main/java/za/ac/iie/mycontacts/MainActivity.kt
@@ -17,8 +17,25 @@ class MainActivity : AppCompatActivity() {
         val dayOfBirth=29
         val monthOfBirth="december"
         val yearOfBirth=2005
+        val currentYear=2026
+        val age= currentYear- yearOfBirth
+
+        val yearsUntil100=100-age
+        val ageInMonths=age*12
+        val ageInDays=age*365
+
+        //1. Remainder when divided by 5
+        val ageRemainder=age%5
+
+        Log.d("Calculation","Years until 100: $yearsUntil100")
+        Log.d("Calculation","Age in months: $ageInMonths")
+        Log.d("Calculation","Age in days: $ageInDays")
+        Log.d("Calculation","Remainder when divided by 5: $ageRemainder")
+
         Log.d("Hi",firstName +" "+lastName+ "you Live in"+ address+ "it is"+ isMale+"that you are male"+dayOfBirth+" "+
         monthOfBirth+" "+yearOfBirth)
+
+
 
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
